36e916b426d5d4800faa9497f00043f2e7d2f785
Tasnia Nowrin <tasnianowrin@tasnias-mbp.home>
2026-07-27 19:54:08 -0400

    first

---
+++ Source/src/Source.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Source is AccessControl {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
+
+    mapping(address => bool) public approved;
+    address[] public tokens;
+
+    event Deposit(
+        address indexed token,
+        address indexed recipient,
+        uint256 amount
+    );
+
+    event Withdrawal(
+        address indexed token,
+        address indexed recipient,
+        uint256 amount
+    );
+
+    event Registration(
+        address indexed token
+    );
+
+    constructor(address admin) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(ADMIN_ROLE, admin);
+        _grantRole(WARDEN_ROLE, admin);
+    }
+
+    function deposit(
+        address _token,
+        address _recipient,
+        uint256 _amount
+    ) public {
+        require(
+            approved[_token],
+            "Token not registered"
+        );
+
+        bool success = ERC20(_token).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
+
+        require(
+            success,
+            "Transfer failed"
+        );
+
+        emit Deposit(
+            _token,
+            _recipient,
+            _amount
+        );
+    }
+
+    function withdraw(
+        address _token,
+        address _recipient,
+        uint256 _amount
+    ) public onlyRole(WARDEN_ROLE) {
+        bool success = ERC20(_token).transfer(
+            _recipient,
+            _amount
+        );
+
+        require(
+            success,
+            "Transfer failed"
+        );
+
+        emit Withdrawal(
+            _token,
+            _recipient,
+            _amount
+        );
+    }
+
+    function registerToken(
+        address _token
+    ) public onlyRole(ADMIN_ROLE) {
+        require(
+            !approved[_token],
+            "Token already registered"
+        );
+
+        approved[_token] = true;
+        tokens.push(_token);
+
+        emit Registration(_token);
+    }
+}
